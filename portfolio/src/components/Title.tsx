53e65de3b3c5c544e5d2167694ca73fac406db01
Ranaldi Armel <armel.ranaldi@gmail.com>
2025-07-08 10:53:08 +0200

    commit about first component

---
+++ portfolio/src/components/Title.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './cssModules/Title.module.css';
+
+interface TitleProps {
+    title: string;
+}
+
+export const Title: React.FC<TitleProps> = ({ title }) => {
+    return (
+        <div className={styles.titleContainer}>
+            <h1 className={styles.title}>{title}</h1>
+        </div>
+    );
+};
