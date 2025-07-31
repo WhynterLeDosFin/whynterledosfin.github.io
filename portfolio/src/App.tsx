094eb72c05e040245250de4e6ccca90238d1a3b4
WhynterLeDosFin <73105244+WhynterLeDosFin@users.noreply.github.com>
2025-07-31 17:43:03 +0200

    Update review from dev (#8)
    * feat: just adding the navbar component first version (#1)
    
    * Section (#2)
    
    * feat: just added sass and css variable file
    
    * feat: working on section component to make it able to fit with the content i need
    
    * Dev update from main (GitHub Page) (#4)
    
    * Update for GitHub page (#3)
    
    * feat: just adding the navbar component first version (#1)
    
    * Section (#2)
    
    * feat: just added sass and css variable file
    
    * feat: working on section component to make it able to fit with the content i need
    
    * fix: wrong push, needed to redo it
    
    * COMPONENT : Separator (#6)
    
    * feat: created a component to split content on my page, a separator
    
    * feat: just added a template for my PR descriptions
    
    * feat: just added a new component to type some characteristic about me
    
    * COMPONENT : Typewriter (#7)
    
    * feat: just added a new component to type some characteristic about me
    
    * fix: just some visual fixes

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Title} from "./components/Title";
+import './App.scss';
+import myPhoto from './assets/img/IMG_6461.jpg';
+import {Navbar} from "./components/Navbar";
+import {Section} from "./components/Section";
+import {theme} from "./styles/theme";
+import {Separator} from "./components/Separator";
+import {Typewriter} from "./components/Typewriter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <Title title={"Portfolio"}/>
-      </header>
+    <Navbar/>
 
+        <Section title={"Bienvenue sur mon portfolio !"} background={theme.colors.gradientBackgroundReversed} fontColor={"#FFFFFF"}>
+            <div className="Intro-container">
+                <div className="Intro-left">
+                   <h1>Enchanté, moi c'est Armel.</h1>
+                   <p>Jeune ingénieur ayant évolué dans le milieu des
+                       technologies de l’information au travers de
+                       différentes formations et divers postes. Je suis
+                       passionné par le développement de solutions
+                       performantes simplifiant les tâches de mes
+                       collaborateurs / clients. Je recherche aujourd’hui
+                       une structure me permettant de faire bon usage de
+                       mes compétences et de continuer de les y développer
+                       dans un cadre dynamique, collaboratif et
+                       bienveillant.
+                   </p>
+                    <p>Je suis actuellement à l'écoute de nouvelles opportunités.
+                    </p>
+               </div>
+                <Separator orientation="vertical" length="400px" thickness="3px" />
+                <div className="Intro-right">
+                    <div className="Picture-container"/>
+                    <div className="Typewriter-container">
+                        <Typewriter texts={["j'ai 27 ans","J'habite à Lille","J'aime développer","J'adore conceptualiser","J'aime le défi (̿▀̿ ̿Ĺ̯̿̿▀̿ ̿)̄","Je suis créatif","j'aime bien la photo aussi :p"]} />
+                    </div>
+                </div>
+            </div>
+        </Section>
+        <Section title={"Mes expériences"} background={theme.colors.gradientBackground} fontColor={"#FFFFFF"}>
+            <p>And that's an other one</p>
+        </Section>
     </div>
   );
 }
