526df088abc7398fc68ff4a706cfe38480cc7695
WhynterLeDosFin <73105244+WhynterLeDosFin@users.noreply.github.com>
2025-10-01 18:25:59 +0200

    Update Review from Dev (#15)
    * feat: just adding the navbar component first version (#1)
    
    LGTM
    
    * Section (#2)
    
    * feat: just added sass and css variable file
    
    * feat: working on section component to make it able to fit with the content i need
    
    * Dev update from main (GitHub Page) (#4)
    
    * Update for GitHub page (#3)
    
    * feat: just adding the navbar component first version (#1)
    
    LGTM
    
    * Section (#2)
    
    * feat: just added sass and css variable file
    
    * feat: working on section component to make it able to fit with the content i need
    
    * fix: wrong push, needed to redo it
    
    * COMPONENT : Separator (#6)
    
    * feat: created a component to split content on my page, a separator
    
    * feat: just added a template for my PR descriptions
    
    * feat: just added a new component to type some characteristic about me
    
    * COMPONENT : Typewriter (#7)
    
    * feat: just added a new component to type some characteristic about me
    
    * fix: just some visual fixes
    
    * COMPONENT : Timeline (#10)
    
    * feat: just add a first version of the Timeline component WIP
    
    * feat: the timeline is up !
    
    * update: just update the build for github page
    
    * fix: changing some synthax error & finally adding the anchor to my navbar
    
    * fix: just fixed my build.sh script
    
    * REFACTO: Navbar (#13)
    
    * fix: just fixed the responsivity of my navbar component and changed the logic of my devs to mobile-first
    
    * update: just update the build and delete some old comments
    
    * COMPONENT : Choices & Progress (#14)
    
    * feat: just added a first version for the choices component W.I.P
    
    * feat: just added some animation to my SVG & some fixes
    
    * feat: just finished the section by polishing Choices and adding progress indicator
    
    * fix: build update for github hosting
    
    * fix: polishing the latests components and my links in the navbar + other fixes

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './cssModules/Navbar.module.scss';
-import {FaGithubSquare, FaLinkedin} from "react-icons/fa";
+import { FaGithubSquare, FaLinkedin, FaBars, FaTimes } from "react-icons/fa";
 import { HiOutlineDocumentDownload } from "react-icons/hi";
 
 export const Navbar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+    const closeMenu = () => setIsOpen(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div className={styles.navbarContainer}>
+        <div className={`${styles.navbarContainer} ${isScrolled ? styles.scrolled : ''}`}>
             <div className={styles.navbarSections}>
-                <a href="#presentation" className={styles.title}>Présentation</a>
-                <a href="#formations" className={styles.title}>Formations</a>
-                <a href="#experiences" className={styles.title}>Expériences</a>
-                <a href="#realisations" className={styles.title}>Réalisations</a>
-                <a href="#metriques" className={styles.title}>Métriques</a>
-                <a href="#projets" className={styles.title}>Projets</a>
-                <a href="#contact" className={styles.title}>Contact</a>
+                <a href="#presentation">Présentation</a>
+                <a href="#formations">Formations</a>
+                <a href="#CePortfolio">Portfolio</a>
+                <a href="#experiences">Expériences</a>
+                <a href="#realisations">Réalisations</a>
+                <a href="#metriques">Métriques</a>
+                <a href="#projets">Projets</a>
+                <a href="#contact">Contact</a>
             </div>
+
+            <div className={styles.burger} onClick={toggleMenu}>
+                {isOpen ? <FaTimes size={28} color="#fff" /> : <FaBars size={28} color="#fff" />}
+            </div>
+
+            <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
+                <a href="#presentation" onClick={closeMenu}>Présentation</a>
+                <a href="#formations" onClick={closeMenu}>Formations</a>
+                <a href="#experiences" onClick={closeMenu}>Expériences</a>
+                <a href="#realisations" onClick={closeMenu}>Réalisations</a>
+                <a href="#metriques" onClick={closeMenu}>Métriques</a>
+                <a href="#projets" onClick={closeMenu}>Projets</a>
+                <a href="#contact" onClick={closeMenu}>Contact</a>
+            </div>
+
+            {isOpen && <div className={styles.overlay} onClick={closeMenu}></div>}
+
             <div className={styles.navbarNetworks}>
-                <a href="https://www.linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin size={42} color="#FFFFFF" className={styles.networkIcon}/>
+                <a href="https://www.linkedin.com/in/armel-ranaldi" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={28} color="#FFFFFF" />
                 </a>
-                <a href="https://www.linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer">
-                    <FaGithubSquare size={42} color="#FFFFFF" className={styles.networkIcon}/>
+                <a href="https://github.com/WhynterLeDosFin" target="_blank" rel="noopener noreferrer">
+                    <FaGithubSquare size={28} color="#FFFFFF" />
                 </a>
-                <a href="https://www.linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer">
-                    <HiOutlineDocumentDownload size={42} color="#FFFFFF" className={styles.networkIcon}/>
+                <a href="/cv.pdf" download="Armel_RANALDI_CV.pdf">
+                    <HiOutlineDocumentDownload size={28} color="#FFFFFF" />
                 </a>
             </div>
         </div>
